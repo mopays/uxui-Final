35461d29ec647e544857296ecd177a690e88aaee
mopays <flokza175@gmail.com>
2024-03-03 12:40:51 +0700

    fix all register

---
+++ assets/script/selectGender.js
@@ -0,0 +1,15 @@
+function selectGender(){
+
+	if($('#prefix').val()=="นาย")
+	{
+		
+		$('#gender2').prop("checked", true);
+		$('#gender1').prop("checked", false);
+	}
+	else
+	{
+		$('#gender2').prop("checked", false);
+		$('#gender1').prop("checked", true);
+	}
+
+}
